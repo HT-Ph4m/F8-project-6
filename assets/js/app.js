bb9aa7ec717024e47eefb42ef358dcc1c30aba85
HT-Ph4m <hongtruongpham18@gmail.com>
2023-09-21 15:43:17 +0700

    update project

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,14 +5,27 @@ const $$ = document.querySelectorAll.bind(document);
 
 const fqaCtaList = $$(".faq__item--cta");
 const fqaItem = $$(".faq__item");
-const slides = $$(".testimonials__item")
-const tabs = $$(".tab__item")
+const slides = $$(".testimonials__item");
+const tabs = $$(".tab__item");
+const pcNav = $("#pc-nav");
+const mobileNav = $("#mobile-nav");
+const overlay = $(".menu-overlay");
+const menuDrawer = $(".menu-drawer");
+const mobileMenuItem = $("#mobile-nav");
+const toggleMenu = $(".toggle-menu");
+const logoInMenu = $(".menu-drawer a");
+const playBtn = $(".statistic__media--play");
+const cardVideo = $(".about-me__media--card");
+const video = $(".statistic__media--video");
+const videoOverlay = $(".media-overlay");
 
 const app = {
+    isPlayVideo: false,
     slideIndex: 0,
-    handleEvents: function(){
+    isShowMenu: false,
+    handleEvents: function () {
         if (fqaCtaList.length > 0) {
-            fqaCtaList.forEach((item,index) => {
+            fqaCtaList.forEach((item, index) => {
                 const fqa = fqaItem[index];
                 item.onclick = () => {
                     const icon = item.querySelector("i");
@@ -25,30 +38,91 @@ const app = {
                         icon.classList.remove("fa-plus");
                         icon.classList.add("fa-minus");
                     }
-                }
+                };
             });
+        }
+
+        toggleMenu.onclick = () => {
+            app.isShowMenu = true;
+            app.toggleMenuMobile();
+        }
+
+        overlay.onclick = () => {
+            app.isShowMenu = false;
+            app.toggleMenuMobile();
+        }
+
+        mobileNav.onclick = () => {
+            app.isShowMenu = false;
+            app.toggleMenuMobile();
+        }
+
+        logoInMenu.onclick = () => {
+            app.isShowMenu = false;
+            app.toggleMenuMobile();
+        }
+
+        playBtn.onclick = () => {
+            video.play();
+            app.isPlayVideo = true;
+            app.watchVideo();
+        }
+
+        videoOverlay.onclick = () => {
+            video.pause();
+            app.isPlayVideo = false;
+            app.watchVideo();
         }
     },
 
-    automaticSlideShow: function(){
+    automaticSlideShow: function () {
         let i;
         app.slideIndex++;
         if (app.slideIndex > slides.length) {
-            app.slideIndex = 1
+            app.slideIndex = 1;
         }
         for (i = 0; i < tabs.length; i++) {
             slides[i].className = slides[i].className.replace(" active", "");
             tabs[i].className = tabs[i].className.replace(" active", "");
-          }
-        slides[app.slideIndex - 1].classList.add("active")
-        tabs[app.slideIndex - 1].classList.add("active")
-        setTimeout(app.automaticSlideShow,3000);
+        }
+        slides[app.slideIndex - 1].classList.add("active");
+        tabs[app.slideIndex - 1].classList.add("active");
+        setTimeout(app.automaticSlideShow, 3000);
     },
 
-    start: function() {
+    generateMenuMobile: function () {
+        mobileNav.innerHTML = pcNav.innerHTML;
+    },
+
+    toggleMenuMobile: function () {
+        if (this.isShowMenu === true) { 
+            overlay.classList.add("active");
+            menuDrawer.classList.add("active");
+        }else {
+            overlay.classList.remove("active");
+            menuDrawer.classList.remove("active");
+        }
+    },
+
+    watchVideo: function () {
+        if (this.isPlayVideo === false) {
+            playBtn.style.display = "block";
+            cardVideo.style.display = "block";
+            videoOverlay.style.display = "none";
+            
+        }else {
+            playBtn.style.display = "none";
+            cardVideo.style.display = "none";
+            videoOverlay.style.display = "block";
+        }
+    },
+
+    start: function () {
+        this.watchVideo();
+        this.generateMenuMobile();
         this.handleEvents();
-        this.automaticSlideShow();//
-    }
-}
+        this.automaticSlideShow();
+    },
+};
 
 app.start();
